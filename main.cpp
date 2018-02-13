cf9e7418a3d7ebaacf0d5312d2fd74f454ed2046
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-13 14:15:57 -0800

    nother save

--- main.cpp
+++ main.cpp
@@ -7,13 +7,22 @@ using namespace std;
 int main()
 {
   //declare variables
-  string first, last; 
+  string first, last, nickname; 
   //get user input
   cout<< "Enter your first name "<<endl;
   cin>> first;
   cout<< "Enter your last name "<<endl;
   cin>> last;
+  nickname.append (first[0]);
   //tell fortune
+  cout<< "Welcome, "<< nickname << "here is your fortune..."<<endl;
+
+
+
+
+
+
+
 
   return 0;
 }
