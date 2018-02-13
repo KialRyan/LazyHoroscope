a98adfc6e9b24268ff74791db07ef46618acd264
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-12 17:20:46 -0800

    Declares first and last name as variables and inputs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Kial Leite Ryan
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,12 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string first, last; 
   //get user input
-
+  cout<< "Enter your first name "<<endl;
+  cin>> first;
+  cout<< "Enter your last name "<<endl;
+  cin>> last;
   //tell fortune
 
   return 0;
