95cbea63efd877702a1b579537d6e04543127532
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-13 18:36:41 -0800

    asks for name inputs and finds lucky #

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:Kial Leite Ryan
+//Authors: Kial Leite Ryan
 #include<iostream>
 #include<string>
 
@@ -6,18 +6,27 @@ using namespace std;
 
 int main()
 {
-  //declare variables
-  string first, last, nickname; 
-  //get user input
-  cout<< "Enter your first name "<<endl;
-  cin>> first;
-  cout<< "Enter your last name "<<endl;
-  cin>> last;
-  nickname.append (first[0]);
-  //tell fortune
-  cout<< "Welcome, "<< nickname << "here is your fortune..."<<endl;
-
-
+    //declare variables
+    string first, last;
+    string nickname; 
+    int lucky;
+    //get user input
+    cout << "Enter your first name " << endl;
+    cin >> first;
+    cout << "Enter your last name " << endl;
+    cin >> last;                                   
+    nickname.push_back (first[0]);
+    nickname.push_back ('.');
+    nickname.push_back (last[0]);
+    nickname.push_back ('.');
+    //tell fortune
+   cout << "Welcome, " << nickname << " here is your fortune..." << endl; 
+   cout << "your lucky number is  " << first.length() << endl;
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
 
 
 
