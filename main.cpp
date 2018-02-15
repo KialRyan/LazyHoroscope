6e6e517eda8ab0691c73589aba5deeaefe87f701
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-15 14:39:52 -0800

    Added last name and first name checks

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     //declare variables
     string first, last;
     string nickname; 
-    int lucky;
+    int lucky, length; 
     //get user input
     cout << "Enter your first name " << endl;
     cin >> first;
@@ -20,9 +20,28 @@ int main()
     nickname.push_back (last[0]);
     nickname.push_back ('.');
     //tell fortune
-   cout << "Welcome, " << nickname << " here is your fortune..." << endl; 
-   cout << "your lucky number is  " << first.length() << endl;
-                                                                    
+    cout << "Welcome, " << nickname << " here is your fortune..." << endl; 
+    cout << "your lucky number is  " << first.length() << endl;
+    if ((first[0] =='a')||(first[0]=='e')||(first[0]=='i')||(first[0]=='o')||
+    (first[0]=='u')||(first[0]=='A')||(first[0]=='E')||(first[0]=='I')||
+    (first[0]   =='O')||(first[0]=='U'))
+    {
+    cout<<"you are destined to be famous!" << endl;
+    }
+    else 
+    {
+    cout<<"you should keep a low profile." <<endl;
+    }
+    length=last.length();
+    if ((last[length-1]='a')||(last[length-1]='e')||(last[length-1]='i')||(last[length-1]='o')||(last[length-1]='u')||(last[length-1]='A')||
+    (last[length-1]='E')||(last[length-1]='I')||(last[length-1]='O')||(last[length-1]='U'))
+    {
+    cout << "you have already met your true love." <<endl;
+    }
+    cout << "have a good day!"<< endl;
+  
+ 
+
                                                                     
                                                                     
                                                                     
