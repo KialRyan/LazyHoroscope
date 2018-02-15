c78532cbbecfbc9a7ff515e5ca462af2db085e40
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-15 15:00:21 -0800

    Fixes issue with = ! being ==

--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ int main()
     string nickname; 
     int lucky, length; 
     //get user input
-    cout << "Enter your first name " << endl;
+    cout << "What is your first name?" << endl;
     cin >> first;
-    cout << "Enter your last name " << endl;
+    cout << "What is your last name?" << endl;
     cin >> last;                                   
     nickname.push_back (first[0]);
     nickname.push_back ('.');
     nickname.push_back (last[0]);
     nickname.push_back ('.');
     //tell fortune
-    cout << "Welcome, " << nickname << " here is your fortune..." << endl; 
-    cout << "your lucky number is  " << first.length() << endl;
+    cout << "Welcome, " << nickname << ", here is your fortune..." << endl; 
+    cout << "your lucky number is " << first.length() << endl;
     if ((first[0] =='a')||(first[0]=='e')||(first[0]=='i')||(first[0]=='o')||
     (first[0]=='u')||(first[0]=='A')||(first[0]=='E')||(first[0]=='I')||
     (first[0]   =='O')||(first[0]=='U'))
@@ -32,9 +32,10 @@ int main()
     {
     cout<<"you should keep a low profile." <<endl;
     }
+    
     length=last.length();
-    if ((last[length-1]='a')||(last[length-1]='e')||(last[length-1]='i')||(last[length-1]='o')||(last[length-1]='u')||(last[length-1]='A')||
-    (last[length-1]='E')||(last[length-1]='I')||(last[length-1]='O')||(last[length-1]='U'))
+    if ((last[length-1]=='a')||(last[length-1]=='e')||(last[length-1]=='i')||(last[length-1]=='o')||(last[length-1]=='u')||(last[length-1]=='A')||
+    (last[length-1]=='E')||(last[length-1]=='I')||(last[length-1]=='O')||(last[length-1]=='U'))
     {
     cout << "you have already met your true love." <<endl;
     }
